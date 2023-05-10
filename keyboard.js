26c98efc6bebd3514bfdb14fa66ad04c14842db4
Chiara Oxender <46496608+coxender@users.noreply.github.com>
2023-05-09 21:02:49 -0400

    fix: :bug: fix backspace and delete, remove cooldown for those

--- keyboard.js
+++ keyboard.js
@@ -1,6 +1,7 @@
 let counter = 0
 let text = ''
 
+// mapping of which keys fire a 1 and which keys fire a 0
 const ZERO = ['1', '2', '3', '4', '5', '6', 'q', 'w', 'e', 'r', 't', 'y', 'a', 's', 'd', 'f', 'g', 'z', 'x', 'c', 'v', 'b']
 const ONE =['7', '8', '9', '0', '-', '=', 'u', 'i', 'o', 'p', ']', ']', '\\', 'h', 'j', 'k', 'l', ';', '\'', 'n', 'm', ',', '.', '/']
 
@@ -8,7 +9,24 @@ let timeWindow = 50; // time in ms
 let lastExecution = new Date((new Date()).getTime() - timeWindow);
 
 
-document.addEventListener("keypress",(event)=>{
+document.addEventListener("keydown",(event)=>{
+    // get key 
+    key = event.key.toLowerCase();
+
+    // get text
+    let textbox= document.querySelector('#textbox')
+    text=textbox.innerHTML
+    if (text === 'Type Here!'){
+        text = ''
+    }
+
+    // let backspace rapid fire
+    if (key === "backspace" || key ==="delete"){
+        counter--;
+        text = text.slice(0,-1)
+    }
+
+    // prevent multiple keys from firing at the same time
     event.preventDefault()
     if (event.repeat) return;
     if ((lastExecution.getTime() + timeWindow) <= (new Date()).getTime()) {
@@ -16,24 +34,13 @@ document.addEventListener("keypress",(event)=>{
     }else{
         return
     }
-
-    let textbox= document.querySelector('#textbox')
-    text=textbox.innerHTML
-    if (text === 'Type Here!'){
-        text = ''
-    }
-
-    key = event.key.toLowerCase()
+    
     if (ZERO.includes(key)){
         text += '0';
         counter++;
     }else if (ONE.includes(key)){
         text += '1';
         counter++;
-    }
-    else if (key === "Backspace" || key ==="Delete"){
-        counter--
-        text = text.slice(0,-1)
     }
 
 
@@ -46,6 +53,6 @@ document.addEventListener("keypress",(event)=>{
         console.log(byte);
         text += byte
     }
-    
+    console.log(counter);
     textbox.innerHTML = text;
 })
