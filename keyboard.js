f7b6861e2439427275dcc500be7595f83e40d1fb
Chiara Oxender <46496608+coxender@users.noreply.github.com>
2023-05-10 19:31:59 -0400

    fix: fix timing issue

--- keyboard.js
+++ keyboard.js
@@ -1,58 +1,106 @@
-let counter = 0
-let text = ''
+let counter = 0;
+let text = "";
 
 // mapping of which keys fire a 1 and which keys fire a 0
-const ONE = ['1', '2', '3', '4', '5', '6', 'q', 'w', 'e', 'r', 't', 'y', 'a', 's', 'd', 'f', 'g', 'z', 'x', 'c', 'v', 'b']
-const ZERO =['7', '8', '9', '0', '-', '=', 'u', 'i', 'o', 'p', ']', ']', '\\', 'h', 'j', 'k', 'l', ';', '\'', 'n', 'm', ',', '.', '/']
+const ONE = [
+  "1",
+  "2",
+  "3",
+  "4",
+  "5",
+  "6",
+  "q",
+  "w",
+  "e",
+  "r",
+  "t",
+  "y",
+  "a",
+  "s",
+  "d",
+  "f",
+  "g",
+  "z",
+  "x",
+  "c",
+  "v",
+  "b",
+];
+const ZERO = [
+  "7",
+  "8",
+  "9",
+  "0",
+  "-",
+  "=",
+  "u",
+  "i",
+  "o",
+  "p",
+  "]",
+  "]",
+  "\\",
+  "h",
+  "j",
+  "k",
+  "l",
+  ";",
+  "'",
+  "n",
+  "m",
+  ",",
+  ".",
+  "/",
+];
 
 let timeWindow = 50; // time in ms
-let lastExecution = new Date((new Date()).getTime() - timeWindow);
+let keysPressed = 0;
 
+document.addEventListener("keydown", (event) => {
+  keysPressed++;
 
-document.addEventListener("keydown",(event)=>{
-    // get key 
-    key = event.key.toLowerCase();
+  // get key
+  key = event.key.toLowerCase();
 
-    // get text
-    let textbox= document.querySelector('#textbox')
-    text=textbox.innerHTML
-    if (text === 'Type Here!'){
-        text = ''
-    }
+  // get text
+  let textbox = document.querySelector("#textbox");
+  text = textbox.innerHTML;
+  if (text === "Type Here!") {
+    text = "";
+  }
 
-    // let backspace rapid fire
-    if (key === "backspace" || key ==="delete"){
-        counter--;
-        text = text.slice(0,-1)
-    }
+  // let backspace rapid fire
+  if (key === "backspace" || key === "delete") {
+    counter--;
+    text = text.slice(0, -1);
+  }
 
-    // prevent multiple keys from firing at the same time
-    event.preventDefault()
-    if (event.repeat) return;
-    if ((lastExecution.getTime() + timeWindow) <= (new Date()).getTime()) {
-        lastExecution = new Date();
-    }else{
-        return
-    }
-    
-    if (ZERO.includes(key)){
-        text += '0';
-        counter++;
-    }else if (ONE.includes(key)){
-        text += '1';
-        counter++;
-    }
+  // prevent multiple keys from firing at the same time
+  event.preventDefault();
+  if (event.repeat) return;
+  if (keysPressed != 1) return;
 
+  if (keysPressed == 1 && ZERO.includes(key)) {
+    text += "0";
+    counter++;
+  } else if (keysPressed == 1 && ONE.includes(key)) {
+    text += "1";
+    counter++;
+  }
 
-    if (counter===8){
-        counter = 0
-        // get last byte
-        let int = parseInt(text.slice(-8),2)
-        let byte = String.fromCharCode(int)
-        text = text.slice(0,-8)
-        console.log(byte);
-        text += byte
-    }
-    console.log(counter);
-    textbox.innerHTML = text;
-})
+  if (counter === 8) {
+    counter = 0;
+    // get last byte
+    let int = parseInt(text.slice(-8), 2);
+    let byte = String.fromCharCode(int);
+    text = text.slice(0, -8);
+    console.log(byte);
+    text += byte;
+  }
+  console.log(counter);
+  textbox.innerHTML = text;
+});
+
+document.addEventListener("keyup", (event) => {
+  keysPressed--;
+});
